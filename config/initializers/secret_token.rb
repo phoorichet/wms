48f9ebd827f75851e7624cded060d99275775358
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2013-09-06 12:41:41 -0700

    Init WMS project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wms::Application.config.secret_token = '6e71354b57c562b2473b3ae69dbd0a75ecf33787f614756ac921b909a2591c45aa0ff1d770978d4b7678cdcb7d849deee8cd5eff5871fdb81a1119475c69f079'
