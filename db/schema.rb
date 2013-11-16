4ce230aa44deaa44f4ddfe42c59cadd6244b13e1
kapil duraphe <kapil.vduraphe@gmail.com>
2013-11-16 03:36:28 -0800

    multi-thread v1

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20131114023905) do
-
-  create_table "analytics", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "storages", :force => true do |t|
     t.datetime "created_at",                       :null => false
